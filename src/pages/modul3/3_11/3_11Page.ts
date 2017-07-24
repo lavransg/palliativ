079bc981169981effaaf491f8f3b0b86bd58965f
Lavrans Stokke Grønseth <lavransg@stud.ntnu.no>
2017-07-24 15:33:22 +0200

    added content to pages

--- src/pages/modul3/3_11/3_11Page.ts
+++ src/pages/modul3/3_11/3_11Page.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
 @Component({
   selector: 'page-3-11',
   templateUrl: '3_11Page.html'
 })
 export class Page3_11 {
 
+  bmi: any;
+  weight: any;
+  height: any;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  updateBmi(){
+    if (this.weight && this.height){
+      this.bmi = (this.weight / ((this.height/100)*(this.height/100))).toFixed(2)
+    }
   }
 
 }
